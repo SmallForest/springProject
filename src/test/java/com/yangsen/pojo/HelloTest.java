ec243315f19999c850b86ec0098a94c36698bb6d
smallforest <1032817724@qq.com>
2022-06-09 13:22:07 +0800

    IOC container

---
+++ src/test/java/com/yangsen/pojo/HelloTest.java
@@ -0,0 +1,20 @@
+package com.yangsen.pojo;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloTest {
+    @Test
+    public void sayHello(){
+        //实例化一个容器
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("ApplicationContext.xml");
+        //创建对象，without new keyword
+        //参数就是Bean的id。最后需要强转
+        Hello hello = (Hello) context.getBean("hello");
+        System.out.println(hello.toString());
+        hello.setMessage("China is a great country!");
+        System.out.println(hello.getMessage());
+    }
+}

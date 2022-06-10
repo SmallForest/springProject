33aaddfa8cb3dcd0e956198a43f1815f7e857dba
smallforest <1032817724@qq.com>
2022-06-10 09:29:30 +0800

    p-namespace

---
+++ src/test/java/com/yangsen/pojo/UserTest.java
@@ -0,0 +1,17 @@
+package com.yangsen.pojo;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class UserTest {
+
+    @Test
+    public void test(){
+        //实例化一个容器
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+        User user = (User) context.getBean("user");
+        System.out.println(user);
+    }
+}
